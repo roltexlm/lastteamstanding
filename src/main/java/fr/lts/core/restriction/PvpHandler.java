5a22410dfd0dc09e816ace5f700ebe7013f96473
roltexlm <roltexlm@users.noreply.github.com>
2026-08-14 18:14:49 +0000

    fix: add missing Hand import in PvpHandler

--- src/main/java/fr/lts/core/restriction/PvpHandler.java
+++ src/main/java/fr/lts/core/restriction/PvpHandler.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
